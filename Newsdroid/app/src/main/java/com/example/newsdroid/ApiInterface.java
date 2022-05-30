a0951a21e867a9e0780f0b50f80fed930dad1487
okybagus871 <91017338+okybagus871@users.noreply.github.com>
2022-05-30 17:25:10 +0700

    EAS PPB

---
+++ Newsdroid/app/src/main/java/com/example/newsdroid/ApiInterface.java
@@ -0,0 +1,22 @@
+package com.example.newsdroid;
+
+import com.example.newsdroid.Model.Headlines;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+    @GET("top-headlines")
+    Call<Headlines> getHeadlines(
+            @Query("country") String country,
+            @Query("apiKey") String apiKey
+    );
+
+    @GET("everything")
+    Call<Headlines> getSpecificData(
+            @Query("q") String query,
+            @Query("apiKey") String apiKey
+    );
+}
---
+++ Newsdroid/app/src/main/java/com/example/newsdroid/WebView.java
@@ -0,0 +1,65 @@
+package com.example.newsdroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.newsapp.R;
+import com.squareup.picasso.Picasso;
+
+public class WebView extends AppCompatActivity {
+
+    android.webkit.WebView webView;
+    ProgressBar loader;
+    TextView tvTitle,tvSource,tvTime,tvDesc;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+//
+        tvTitle = findViewById(R.id.tvTitle);
+        tvSource = findViewById(R.id.tvSource);
+        tvTime = findViewById(R.id.tvDate);
+        tvDesc = findViewById(R.id.tvDesc);
+
+        imageView = findViewById(R.id.imageView);
+
+        webView = findViewById(R.id.webView);
+
+        loader = findViewById(R.id.webViewLoader);
+        loader.setVisibility(View.VISIBLE);
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+        String source = intent.getStringExtra("source");
+        String time = intent.getStringExtra("time");
+        String desc = intent.getStringExtra("desc");
+        String imageUrl = intent.getStringExtra("imageUrl");
+        String url = intent.getStringExtra("url");
+
+        tvTitle.setText(title);
+        tvSource.setText(source);
+        tvTime.setText(time);
+        tvDesc.setText(desc);
+
+        Picasso.with(WebView.this).load(imageUrl).into(imageView);
+
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+        if (webView.isShown()){
+            loader.setVisibility(View.INVISIBLE);
+        }
+    }
+}
